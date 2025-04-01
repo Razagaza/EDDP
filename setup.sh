c19c4f79c55efd91bcc351fbfaef5aac39f1cd81
Jaekwon Shin <80142827+Razagaza@users.noreply.github.com>
2025-04-01 14:49:34 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,21 +19,23 @@ if [[ ! -f ~/CASTEP-25.11.tar.gz ]]; then
 fi
 
 cd && wget https://ftp.gnu.org/gnu/make/make-4.2.1.tar.gz
-tar -xf make-4.2.1.tar.gz && cd make-4.2.1/
+tar -xvf make-4.2.1.tar.gz && cd make-4.2.1/
 ./configure --prefix=$PWD
 make && make install
 echo 'export PATH=$HOME/make-4.2.1/bin:$PATH' >> ~/.bashrc
 echo 'export CPATH=$HOME/make-4.2.1/include:$CPATH' >> ~/.bashrc
-source ~/.bashrc
+export PATH=$HOME/make-4.2.1/bin:$PATH
+export CPATH=$HOME/make-4.2.1/include
 
-cd && tar -xzf gcc-13.1.0.tar.gz
+cd && tar -xvzf gcc-13.1.0.tar.gz
 echo 'export PATH=$HOME/gcc-13.1.0/bin:$PATH' >> ~/.bashrc
 echo 'export CPATH=$HOME/gcc-13.1.0/include:$CPATH' >> ~/.bashrc
 echo 'export LIBRARY_PATH=$HOME/gcc-13.1.0/lib:$HOME/gcc-13.1.0/lib64:$LIBRARY_PATH' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=$HOME/gcc-13.1.0/lib:$HOME/gcc-13.1.0/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
-set +e
-source ~/.bashrc
-set -e
+export PATH=$HOME/gcc-13.1.0/bin:$PATH
+export CPATH=$HOME/gcc-13.1.0/include:$CPATH
+export LIBRARY_PATH=$HOME/gcc-13.1.0/lib:$HOME/gcc-13.1.0/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/gcc-13.1.0/lib:$HOME/gcc-13.1.0/lib64
 
 cd && git clone https://github.com/Reference-LAPACK/lapack.git && cd lapack/
 mkdir build && cd build
@@ -42,16 +44,17 @@ cmake -DCMAKE_INSTALL_PREFIX=$HOME/lapack ..
 cmake --build . -j --target install
 echo 'export LD_LIBRARY_PATH=$HOME/lapack/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
 echo 'export LIBRARY_PATH=$HOME/lapack/lib64:$LIBRARY_PATH' >> ~/.bashrc
-source ~/.bashrc
+export LD_LIBRARY_PATH=$HOME/lapack/lib64:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$HOME/lapack/lib64:$LIBRARY_PATH
 
 cd && wget https://ftp.gnu.org/gnu/parallel/parallel-20220222.tar.bz2
-tar -xf parallel-20220222.tar.bz2 && cd parallel-20220222/
+tar -xvf parallel-20220222.tar.bz2 && cd parallel-20220222/
 ./configure --prefix=$PWD
 make && make install
 echo 'export PATH=$HOME/parallel-20220222/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+export PATH=$HOME/parallel-20220222/bin:$PATH
 
-cd && tar -xf eddp-v0.2.tgz && cd eddp
+cd && tar -xvf eddp-v0.2.tgz && cd eddp
 cd airss
 make && make install
 cd ../nn
@@ -62,16 +65,20 @@ cd ../ddp
 make && make install
 echo 'export EDDP_HOME=$HOME/eddp' >> ~/.bashrc
 echo 'export PATH=$EDDP_HOME/airss/bin:$EDDP_HOME/nn/bin:$EDDP_HOME/repose/bin:$EDDP_HOME/ddp/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+export EDDP_HOME=$HOME/eddp
+export PATH=$EDDP_HOME/airss/bin:$EDDP_HOME/nn/bin:$EDDP_HOME/repose/bin:$EDDP_HOME/ddp/bin:$PATH
 
 # ddp-batch.tar 파일 업로드하고 진행
-cd && tar -xf ddp-batch.tar
+cd && tar -xvf ddp-batch.tar
 echo 'export PATH=$HOME/ddp-batch/bin:$PATH' >> ~/.bashrc
-source ~/.bashrc
+export PATH=$HOME/ddp-batch/bin:$PATH
 
 # CASTEP-25.11.tar.gz 파일 업로드하고 진행
-cd && tar -xzf CASTEP-25.11.tar.gz
+cd && tar -xvzf CASTEP-25.11.tar.gz
 echo 'module load intel/19.0.5 impi/19.0.5' >> ~/.bashrc
 echo 'export CASTEP=$HOME/CASTEP-25.11' >> ~/.bashrc
 echo 'export PATH=$CASTEP/bin:$CASTEP/bin/linux_x86_64_ifort--serial:$CASTEP/obj/linux_x86_64_ifort--mpi:$PATH'  >> ~/.bashrc
-source ~/.bashrc
+module load intel/19.0.5 impi/19.0.5
+export CASTEP=$HOME/CASTEP-25.11
+export PATH=$CASTEP/bin:$CASTEP/bin/linux_x86_64_ifort--serial:$CASTEP/obj/linux_x86_64_ifort--mpi:$PATH
+~                                                                                                                         
